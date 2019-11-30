74ea73f25bcf8048d855fb1928dc99c93b9db961
unknown <edvard.gogidze.1@btu.edu.ge>
2019-11-29 23:05:32 -0800

    update

--- Java_davaleba_4/Main.java
+++ Java_davaleba_4/Main.java
@@ -17,19 +17,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         parametrs = sc.nextLine();
         String[] par = parametrs.split("-");
-        int number_of_p = Integer.parseInt(par[1]);
-        int start_number_of_s = Integer.parseInt(par[3]);
-        int end_number_of_s = Integer.parseInt(par[4]);
-        int start_number_of_w = Integer.parseInt(par[6]);
-        int end_number_of_w = Integer.parseInt(par[7]);
+
 
         Main n = new Main();
+
+
+        int number_of_p = n.checkIfexist(1,2,par);
+        int start_number_of_s = n.checkIfexist(3,5,par);
+        int end_number_of_s = n.checkIfexist(4,7,par);
+        int start_number_of_w = n.checkIfexist(6,7,par);
+        int end_number_of_w = n.checkIfexist(7,9,par);
+
+        
         for(int i = 0; i<number_of_p; i++)
         {
             String stringLang = n.generateAlf(language);
             n.generateWord(start_number_of_w,end_number_of_w,start_number_of_s,end_number_of_s, stringLang);
         }
 
+    }
+
+    public int checkIfexist(int index, int defaultVal, String [] par)
+    {
+        try {
+           return Integer.parseInt(par[index]);
+        } catch ( IndexOutOfBoundsException e ) {
+            
+        } catch ( NumberFormatException e ) {
+            
+        }
+        return defaultVal;
     }
 
     public String generateAlf(String lang)
